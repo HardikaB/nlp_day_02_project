c4862836b4682b0274944d4afeec68394993dd38
HardikaB <hardi.2607@gmail.com>
2018-08-28 16:24:18 +0000

    Done

--- q01_load_data_tfidf/build.py
+++ q01_load_data_tfidf/build.py
@@ -7,10 +7,10 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 # Write your solution here :
 
 def q01_load_data_tfidf(path,max_df=0.5,min_df=2,no_features=1000):
-    df=pd.read_csv(path)
+    variable1=pd.read_csv(path)
     tf_vect=TfidfVectorizer(max_df=max_df,min_df=min_df,max_features=no_features)
-    tt_feature=tf_vect.fit_transform(df['talkTitle'])
+    variable2=tf_vect.fit_transform(variable1['talkTitle'])
     variable3=tf_vect.get_feature_names()
-    return df,tt_feature,variable3
+    return variable1,variable2,variable3
 
 
--- q03_LDA/build.py
+++ q03_LDA/build.py
@@ -1,3 +1,4 @@
+# %load q03_LDA/build.py
 # Default imports
 
 from sklearn.decomposition import LatentDirichletAllocation
@@ -5,6 +6,14 @@ from greyatomlib.nlp_day_02_project.q02_count_vectorizer_for_LDA.build import q0
 
 
 # Write your solution here :
+def q03_LDA(path):
+    tf,v2=q02_count_vectorizer_for_LDA(path)
+    
+    lda = LatentDirichletAllocation(n_components=20, max_iter=500,learning_method='batch',random_state=1)
+    lda.fit(tf)
+    return lda.components_
 
 
+#path='data/sessions.csv'
+#q03_LDA(path)
 
