e66d206a755f4b65c1ba64688aa52fd8737daec9
HardikaB <hardi.2607@gmail.com>
2018-08-28 15:42:16 +0000

    Done

--- q01_load_data_tfidf/build.py
+++ q01_load_data_tfidf/build.py
@@ -1,3 +1,4 @@
+# %load q01_load_data_tfidf/build.py
 # Default imports
 
 import pandas as pd
@@ -5,6 +6,11 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 
 # Write your solution here :
 
-
+def q01_load_data_tfidf(path,max_df=0.5,min_df=2,no_features=1000):
+    df=pd.read_csv(path)
+    tf_vect=TfidfVectorizer(max_df=max_df,min_df=min_df,max_features=no_features)
+    tt_feature=tf_vect.fit_transform(df['talkTitle'])
+    variable3=tf_vect.get_feature_names()
+    return df,tt_feature,variable3
 
 
--- q02_count_vectorizer_for_LDA/build.py
+++ q02_count_vectorizer_for_LDA/build.py
@@ -1,3 +1,4 @@
+# %load q02_count_vectorizer_for_LDA/build.py
 # Default imports
 
 import pandas as pd
@@ -8,5 +9,13 @@ from greyatomlib.nlp_day_02_project.q01_load_data_tfidf.build import q01_load_da
 from sklearn.feature_extraction.text import CountVectorizer
 
 # Write your solution here:
-
+def q02_count_vectorizer_for_LDA(path):
+    df,tf_talktitle,tf_feat=q01_load_data_tfidf(path,0.5,2,1000)
+    #X_train,X_test,y_train,y_test = train_test_split(df[:,:-1],df[:,-1],test_size = 0.3)
+    c_vect=CountVectorizer(analyzer='word',ngram_range=(1, 1),min_df=0,stop_words='english')
+    variable1=c_vect.fit_transform(df['talkTitle'])
+    variable2=c_vect.get_feature_names()
+    return variable1,variable2
+#path='data/sessions.csv'
+#q02_count_vectorizer_for_LDA(path)
 
