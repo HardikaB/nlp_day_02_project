b8f7e77c100949100b2fcc5953d82c89c82f79b4
HardikaB <hardi.2607@gmail.com>
2018-08-30 10:36:36 +0000

    Done

--- q01_load_data_tfidf/build.py
+++ q01_load_data_tfidf/build.py
@@ -6,9 +6,9 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 
 # Write your solution here :
 
-def q01_load_data_tfidf(path,max_df=0.5,min_df=2,no_features=1000):
+def q01_load_data_tfidf(path,max_df=0.95,min_df=2,no_features=1000):
     variable1=pd.read_csv(path)
-    tf_vect=TfidfVectorizer(max_df=max_df,min_df=min_df,max_features=no_features)
+    tf_vect=TfidfVectorizer(stop_words='english',max_df=max_df,min_df=min_df,max_features=no_features)
     variable2=tf_vect.fit_transform(variable1['talkTitle'])
     variable3=tf_vect.get_feature_names()
     return variable1,variable2,variable3
--- q04_topic_modelling/build.py
+++ q04_topic_modelling/build.py
@@ -1,3 +1,4 @@
+# %load q04_topic_modelling/build.py
 # Default imports
 
 import numpy as np
@@ -6,6 +7,18 @@ from greyatomlib.nlp_day_02_project.q03_LDA.build import q03_LDA
 
 
 #  Write your solution here :
+def q04_topic_modelling(path,n_top_words=20):
+    v1,v2=q02_count_vectorizer_for_LDA(path)
+    v3=q03_LDA(path)
+    message=list()
+    for topic_idx, topic in enumerate(v3):
+        message =[v2[i] for i in topic.argsort()[:-n_top_words - 1:-1]]
+    return message
 
 
+    
+
+
+#path='data/sessions.csv'
+#q04_topic_modelling(path,20)
 
